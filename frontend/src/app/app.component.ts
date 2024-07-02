e42a7d58d366d68f2be30f10d20f20275ea03048
tr3sar <josepcdg@gmail.com>
2024-07-02 21:40:47 +0200

    Eliminar codi demostració iot

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -10,29 +10,10 @@ import { Room } from './models/room.model';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  clients: Client[] = [];
-  rooms: Room[] = [];
-
-  clientsNames = ['John', 'Jane', 'Doe', 'Smith', 'Brown', 'Wilson', 'Johnson', 'Williams', 'Jones', 'Miller'];
 
   constructor(private clientService: ClientService, private roomService: RoomService) {}
 
   ngOnInit() {
     
-  }
-
-  addClient() {
-    if (this.clients.length >= this.clientsNames.length - 1) return;
-    const newClient = { id: this.clients.length + 1, name: this.clientsNames[this.clients.length + 1] };
-    this.clientService.createClient(newClient).subscribe(response => {
-      this.clients.push(response);
-    });
-  }
-
-  addRoom() {
-    const newRoom: Room = { number: this.rooms.length + 1};
-    this.roomService.createRoom(newRoom).subscribe(response => {
-      this.rooms.push(response);
-    });
   }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,18 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ClientComponent } from './components/client/client.component';
-import { RoomComponent } from './components/room/room.component';
-import { MenuComponent } from './components/menu/menu.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClientComponent,
-    RoomComponent,
-    MenuComponent
   ],
   imports: [
     BrowserModule,

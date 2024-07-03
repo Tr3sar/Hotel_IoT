4b40c1ebfb5f6654f46f67b5e5245676050af826
tr3sar <josepcdg@gmail.com>
2024-07-03 17:13:48 +0200

    llevar endpoint q no es de simulació

--- backend/app/routers_simulation/simulation_rooms.py
+++ backend/app/routers_simulation/simulation_rooms.py
@@ -7,10 +7,6 @@ def get_storage():
     from app.main import storage
     return storage
 
-@router.put("/{room_number}/status", response_model=schemas.Room)
-def update_room_status(room_number: int, request: schemas.UpdateRoomStatusRequest, storage: storage_module.Storage = Depends(get_storage)):
-    return storage.update_room_status(room_number=room_number, status=request.status)
-
 @router.put("/{room_number}/environment", response_model=dict)
 def adjust_environment(room_number: int, request: schemas.AdjustEnvironmentRequest, storage: storage_module.Storage = Depends(get_storage)):
     res = storage.adjust_environment(room_number=room_number, temperature=request.temperature, lighting_intensity=request.lighting_intensity)
